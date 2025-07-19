3208219af90d4cfbcc97a47ace6460e25d090259
ElyseMahefaniaina <119030086+Elyse-Mahefaniaina@users.noreply.github.com>
2025-07-20 02:46:37 +0300

    restructure

--- truck_api/truck_api/settings.py
+++ truck_api/truck_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-visuexfx=g0b+zjrxs&v=rkmv=f7xt&*pr=#6jv60-^44jec@g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
