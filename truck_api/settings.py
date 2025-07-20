601fe8a769bc012652215698eecdf452273418ca
ElyseMahefaniaina <119030086+Elyse-Mahefaniaina@users.noreply.github.com>
2025-07-20 04:42:45 +0300

    cors

--- truck_api/settings.py
+++ truck_api/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS  = [
-    "http://localhost:3000"
+    "https://truck-front-azure.vercel.app"
 ]
 
 
